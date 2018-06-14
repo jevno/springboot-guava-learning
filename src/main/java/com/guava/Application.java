267fbc69a2ad75e8b536a70a8a1cf56547215210
jevno <rucky2013@163.com>
2018-06-14 16:23:28 +0800

    代码初次提交

---
+++ src/main/java/com/guava/Application.java
@@ -0,0 +1,22 @@
+package com.guava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @title 		
+ * @description	
+ * @usage		
+ * @copyright	Copyright 2017  marsmob Corporation. All rights reserved.
+ * @company		marsmob
+ * @author		jevno
+ * @create		2018年4月29日 下午5:57:33
+ */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
