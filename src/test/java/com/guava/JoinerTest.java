6bbf343d7562b5eaeaa70f67c28b6875c3a62eba
jevno <rucky2013@163.com>
2018-06-15 17:41:14 +0800

    Joiner Test

--- src/test/java/com/guava/JoinerTest.java
+++ src/test/java/com/guava/JoinerTest.java
@@ -1,14 +1,23 @@
 package com.guava;
 
+import static java.util.stream.Collectors.joining;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Files;
 
 /**
  * 
@@ -26,6 +35,13 @@ public class JoinerTest {
 	
 	private final List<String> stringListWithNullValue = Arrays.asList("Google", "Guava", "Java", "Scala", null);
 	
+	private final String targetFileName = "D:\\stswork\\github\\springboot-guava-learning\\guava-joiner.txt";
+	
+	private final String targetFileNameToMap = "D:\\stswork\\github\\springboot-guava-learning\\guava-joiner-map.txt";
+	
+	private final Map<String,Object> stringMap = ImmutableMap.of("name","jevno","age", 28);
+			
+	
 	@Test
 	public void testJoinOnJoin()
 	{
@@ -40,6 +56,13 @@ public class JoinerTest {
 		assertThat(result, equalTo("Google#Guava#Java#Scala#Groovy"));
 	}
 	
+	/**
+	* @Title: testJoinOnJoinWithNullValueButSkip
+	* @Description: DO(忽略空值)
+	* @param     参数
+	* @return void    返回类型
+	* @throws
+	 */
 	@Test
 	public void testJoinOnJoinWithNullValueButSkip()
 	{
@@ -47,10 +70,105 @@ public class JoinerTest {
 		assertThat(result, equalTo("Google#Guava#Java#Scala"));
 	}
 	
+	/**
+	 * 
+	* @Title: testJoin_On_Join_WithNullValue_UseDefaultValue
+	* @Description: DO(对null进行替换操作)
+	* @param     参数
+	* @return void    返回类型
+	* @throws
+	 */
 	@Test
 	public void testJoin_On_Join_WithNullValue_UseDefaultValue()
 	{
 		String result = Joiner.on("#").useForNull("DEFAULT").join(stringListWithNullValue);
 		assertThat(result, equalTo("Google#Guava#Java#Scala#DEFAULT"));
+	}
+	
+	/**
+	 * 
+	* @Title: testJoin_On_Append_To_StringBuilder
+	* @Description: DO(动态拼接)
+	* @param     参数
+	* @return void    返回类型
+	* @throws
+	 */
+	@Test
+	public void testJoin_On_Append_To_StringBuilder()
+	{
+		final StringBuilder builder = new StringBuilder();
+		StringBuilder resultBuilder = Joiner.on("#").useForNull("DEFAULT").appendTo(builder, stringListWithNullValue);
+		assertThat(resultBuilder, sameInstance(builder));
+		assertThat(resultBuilder.toString(), equalTo("Google#Guava#Java#Scala#DEFAULT"));
+		assertThat(builder.toString(), equalTo("Google#Guava#Java#Scala#DEFAULT"));
+	}
+	
+	/**
+	 * 
+	* @Title: testJoin_On_Append_To_Writer
+	* @Description: DO(appendTo 文件写入)
+	* @param     参数
+	* @return void    返回类型
+	* @throws
+	 */
+	@Test
+	public void testJoin_On_Append_To_Writer()
+	{
+		try(FileWriter writer = new FileWriter(new File(targetFileName)))
+		{
+			Joiner.on("#").useForNull("DEFAULT").appendTo(writer, stringListWithNullValue);
+			assertThat(Files.isFile().test(new File(targetFileName)), equalTo(true));
+		}catch (IOException e) {
+			fail("append to the writer occur fetal error.");
+		};
+	}
+	
+	/**
+	* @Title: testJoiningByStreamSkipNullValues
+	* @Description: DO(jdk8 join)
+	* @param     参数
+	* @return void    返回类型
+	* @throws
+	 */
+	@Test
+	public void testJoiningByStreamSkipNullValues()
+	{
+		String result = stringListWithNullValue.stream().filter(item -> item != null && !item.isEmpty()).collect(joining("#"));
+		assertThat(result, equalTo("Google#Guava#Java#Scala"));
+	}
+	
+	/**
+	* @Title: testJoiningByStreamWithDefaultValue
+	* @Description: DO(jdk8 join with default)
+	* @param     参数
+	* @return void    返回类型
+	* @throws
+	 */
+	@Test
+	public void testJoiningByStreamWithDefaultValue()
+	{
+		String result = stringListWithNullValue.stream()
+							.map(item -> item==null || item.isEmpty() ? "DEFAULT" : item)
+							.collect(joining("#"));
+		assertThat(result, equalTo("Google#Guava#Java#Scala#DEFAULT"));
+	}
+	
+	@Test
+	public void testJoinOnMapJoin()
+	{
+		String result = Joiner.on(";").withKeyValueSeparator("=").join(stringMap);
+		assertThat(result, equalTo("name=jevno;age=28"));
+	}
+	
+	@Test
+	public void testJoinOnMapJoinToAppendable()
+	{
+		try(FileWriter writer = new FileWriter(new File(targetFileNameToMap)))
+		{
+			Joiner.on(";").withKeyValueSeparator("=").appendTo(writer, stringMap);
+			assertThat(Files.isFile().test(new File(targetFileNameToMap)), equalTo(true));
+		}catch (IOException e) {
+			fail("append to the writer occur fetal error.");
+		};
 	}
 }
