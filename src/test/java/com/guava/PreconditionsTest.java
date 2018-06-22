14efdb01cf71f3464e0bcceedf9edad9ee6e191c
jevno <rucky2013@163.com>
2018-06-22 18:18:50 +0800

    Preconditions

--- src/test/java/com/guava/PreconditionsTest.java
+++ src/test/java/com/guava/PreconditionsTest.java
@@ -1,8 +1,14 @@
 package com.guava;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
 
 import org.junit.Test;
+
+import com.google.common.base.Preconditions;
 
 /**
  * @title 		Preconditions Test 前置判断（断言）
@@ -21,5 +27,52 @@ public class PreconditionsTest {
 		checkNotNull(null);
 	}
 	
+	@Test
+	public void testCheckNotNullWithMessage()
+	{
+		try {
+			checkNotNullWithMessage(null);
+		} catch (Exception e) {
+//			assertThat(e, is(NullPointerException.class));
+			assertThat(e.getMessage(), equalTo("The list should not be null"));
+		}
+	}
 	
+	@Test
+	public void testCheckNotNullWithFormatMessage()
+	{
+		try {
+			checkNotNullWithFormatMessage(null);
+		} catch (Exception e) {
+//			assertThat(e, is(NullPointerException.class));
+			assertThat(e.getMessage(), equalTo("The list should not be null and the size must be 2"));
+		}
+	}
+	
+	@Test
+	public void testCheckArguments()
+	{
+		final String type = "A";
+		
+		try {
+			Preconditions.checkArgument(type.equals("B"));
+		} catch (Exception e) {
+			//assertThat(e, is(IllegalArgumentException.class));
+		}
+	}
+	
+	private void checkNotNull(final List<String> list)
+	{
+		Preconditions.checkNotNull(list);
+	}
+	
+	private void checkNotNullWithMessage(final List<String> list)
+	{
+		Preconditions.checkNotNull(list, "The list should not be null");
+	}
+	
+	private void checkNotNullWithFormatMessage(final List<String> list)
+	{
+		Preconditions.checkNotNull(list, "The list should not be null and the size must be %s", 2);
+	}
 }
